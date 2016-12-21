ff5007ccde381131ac96fc7eca90ad32f9855952
JoramUiterwijk <joram1998@live.nl>
2016-12-21 17:10:48 +0100

    new project

---
+++ MetalGearSolid/metalGear/Assets/_Scripts/Movement/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float y = 0f;
+
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 250.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, y, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            y = 1f;
+        }
+        else
+        {
+            y = 0f;
+        }
+    }
+
+    void OnCollisionEnter(Collision other) 
+    {
+        if (other.gameObject.tag == ("Stairs"))
+        {
+            y += 1;
+            Debug.Log("Up!");
+        }
+        else
+        {
+            y = 0f;
+        }
+    }
+}
---
+++ MetalGearSolid/metalGear/Assets/_Scripts/Test/CameraTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTest : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject target;
+    [SerializeField]
+    private float distance;
+
+	void Update ()
+    {
+		if(target != null)
+        {
+            this.transform.position = target.transform.position + new Vector3(0, 0, distance);
+            this.transform.rotation = target.transform.rotation;
+        }
+	}
+    public void otherPlayer(GameObject player)
+    {
+        target = player;
+    }
+}
