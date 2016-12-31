0eda93beb7b8a5f721fb666a777c98f41c80dc28
Danivdwerf <danivdwerf.com>
2016-12-31 16:52:55 +0100

    bullet images

---
+++ Assets/_Scripts/Camera_Movement/CameraRegulator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRegulator : MonoBehaviour
+{
+    [SerializeField]
+    private string[] tags;
+
+    [SerializeField]
+    private GameObject cam;
+    private MoveCamera moveCamera;
+
+	private void Start ()
+    {
+        moveCamera = cam.GetComponent<MoveCamera>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if(tags.Length - 1 >= 0)
+        {
+            if (other.CompareTag(tags[0]))
+            {
+                setNewPosition(other.gameObject);
+            }
+        }
+        else
+        {
+            Debug.Log("no tags asignt");
+        }
+        
+    }
+
+    private void setNewPosition(GameObject other)
+    {
+        Transform child = other.transform.Find("NewPosition");
+        moveCamera.newPosition(child);
+    }
+}
---
+++ Assets/_Scripts/Camera_Movement/MoveCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 2;
+
+    private Vector3 targetPosition;
+    private Vector3 velocity;
+    private Quaternion targetRotation;
+
+	public void newPosition (Transform newTrans)
+    {
+        targetPosition = newTrans.position;
+        velocity = newTrans.position - this.gameObject.transform.position;
+        velocity.Normalize();
+        velocity = velocity * speed;
+        //targetPosition = newTrans.position;
+        targetRotation = newTrans.localRotation;
+	}
+
+	void Update ()
+    {
+        if(Vector3.Distance(targetPosition,this.gameObject.transform.position) > 0.1)
+        smoothMove();
+    }
+
+    private void smoothMove()
+    {
+        this.gameObject.transform.position += velocity * Time.time;
+        this.gameObject.transform.localRotation = targetRotation;
+    }
+}
