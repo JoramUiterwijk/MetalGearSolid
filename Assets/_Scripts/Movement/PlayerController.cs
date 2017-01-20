00c70528c59ea32b5f8aa29a3aa49fb4f2731280
JoramUiterwijk <joram1998@live.nl>
2017-01-20 14:39:21 +0100

    Movement

--- Assets/_Scripts/Movement/PlayerController.cs
+++ Assets/_Scripts/Movement/PlayerController.cs
@@ -11,17 +11,43 @@ public class PlayerController : MonoBehaviour
     private float speed = 10;
     private float dash = 2.0f;
 
+    private float moveHorizontal = 0;
+    private float moveVertical = 0;
+    private Vector3 previousMovement = Vector3.zero;
+
+
+    public Vector3 eulerAngleVelocity;
+
     void Awake()
     {
         rigidBody = GetComponent<Rigidbody>();
     }
-
+		
     void Update()
     {
-        x = Input.GetAxisRaw("Horizontal");
+        Movement();
+
+        /*x = Input.GetAxisRaw("Horizontal");
         z = Input.GetAxisRaw("Vertical");
 
-        movement = transform.forward.normalized * z;
+        movement = transform.forward.normalized * z;*/
+    }
+
+    private void Movement()
+    {
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+        
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(previousMovement), 0.10F);
+
+        transform.Translate(movement * 5 * Time.deltaTime, Space.World);
+
+        if(!(movement.x == 0 && movement.z == 0))
+        {
+            previousMovement = movement;
+        }
+        
     }
 
     void FixedUpdate()
@@ -29,6 +55,8 @@ public class PlayerController : MonoBehaviour
         Vector3 velocity = movement * speed * Time.fixedDeltaTime;
         rigidBody.rotation = Quaternion.Euler(rigidBody.rotation.eulerAngles + new Vector3(0f, 2 * x, 0f));
         rigidBody.MovePosition(rigidBody.position + velocity);
-        //rigidBody.AddForce(Physics.gravity * rigidBody.mass);
+
+		Quaternion deltaRotation = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime);
+		rigidBody.MoveRotation(rigidBody.rotation * deltaRotation);
     }
 }
