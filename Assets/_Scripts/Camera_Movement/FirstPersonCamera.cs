54dfea83db321365a040238f619883ba91871165
brianboersen <19965@ma-web.nl>
2017-01-24 14:09:32 +0100

    first person camera

--- Assets/_Scripts/Camera_Movement/FirstPersonCamera.cs
+++ Assets/_Scripts/Camera_Movement/FirstPersonCamera.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class FirstPersonCamera : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private Vector3 _lastCameraPosition;
+    private Quaternion _lastCameraRotation;
+    //private Transform _lastCameraTransform;
+
+    [SerializeField]
+    private GameObject _PlayerEye;
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public void setFirstPerson ()
+    {
+        _lastCameraPosition = gameObject.transform.position;
+        _lastCameraRotation = gameObject.transform.localRotation;
+      
+        gameObject.transform.position = _PlayerEye.transform.position;
+        gameObject.transform.localRotation = _PlayerEye.transform.parent.localRotation;
+        transform.parent = _PlayerEye.transform;
+    }
+
+    public void stopFirstPerson()
+    {
+        transform.parent = null;
+        gameObject.transform.position = _lastCameraPosition;
+        gameObject.transform.localRotation = _lastCameraRotation;      
+    }
 }
