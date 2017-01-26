6d4537a9473ec505dc35d9e6bf25983f52574efe
JoramUiterwijk <joram1998@live.nl>
2017-01-26 11:17:01 +0100

    Testing scene

--- Assets/_Scripts/Camera_Movement/FirstPersonCameraRotate.cs
+++ Assets/_Scripts/Camera_Movement/FirstPersonCameraRotate.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class FirstPersonCameraRotate : MonoBehaviour
 {
-
     public float moveSpeed = 20.0f;
     public bool invert;
     private float hozTurn = 0.0f;
     private float verTurn = 0.0f;
+    float minRotation = -0.45f;
+    float maxRotation = 45f;
+    private float currentRotation;
+
+    void Start()
+    {
+        transform.localRotation = Quaternion.identity;
+    }
 
     void Update()
     {
@@ -17,13 +24,26 @@ public class FirstPersonCameraRotate : MonoBehaviour
         {
             invertVal = -1;
         }
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            //transform.Rotate(verTurn, 0, 0) * moveSpeed * Time.deltaTime;
-        }
-        //verTurn = Input.GetAxis("CamRotVer") * moveSpeed * Time.deltaTime * invertVal;
-        //transform.Rotate(verTurn, hozTurn, 0);
 
-        //transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
+        currentRotation = transform.localRotation.x;
+
+        hozTurn = Input.GetAxis("CamRotHor") * moveSpeed * Time.deltaTime;
+        transform.Rotate(hozTurn * Vector3.up, Space.World);
+
+        print(currentRotation);
+        if (currentRotation > minRotation && currentRotation < maxRotation)
+        {
+            verTurn = Input.GetAxis("CamRotVer") * moveSpeed * Time.deltaTime * invertVal;
+            transform.Rotate(verTurn * Vector3.right, Space.Self);
+        }
+        else
+        {
+            currentRotation = -0.45f;
+        }
+
+        /*Vector3 currentRot = transform.localRotation.eulerAngles;
+        currentRot.x =  Mathf.Clamp(currentRot.x, minRotation, maxRotation);
+        transform.localRotation = Quaternion.Euler(currentRot);*/
     }
+
 }
